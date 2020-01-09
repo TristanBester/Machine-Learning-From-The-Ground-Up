8b1e6efb46bce5348cc50865b236855f1b26f52c
TristanBester <tristanbester@gmail.com>
2020-01-09 21:38:24 +0200

    Began refactoring decision_tree.py and created decision_tree_example.py

--- Decision Tree/decision_tree.py
+++ Decision Tree/decision_tree.py
@@ -1,32 +1,81 @@
-from sklearn.datasets import make_blobs
+# Created by Tristan Bester
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-
-
 
 class Node(object):
-    def __init__(self, val, decision, classes):
-        self.gini = val
+    '''
+    Objects of this class serve as single nodes in the decision trees created by
+    the Tree class defined below. The objects function to store the necessary 
+    information required by a node in a decision tree.
+    
+    Args:
+        gini (float): The Gini impurity of the node.
+        decision (tuple): Stores the feature and threshold used for the decision
+                          at the node. Format: (threshold, feature index).
+        classes (numpy.ndarray): The number of samples in each class for the instances
+                                 to which the node applies.
+    
+    Attributes:
+        gini (float): The Gini impurity of the node.
+        decision (tuple): The information used to make the decision at the node.
+        classes (numpy.ndarray): The number of samples in each class for the instances
+                                 to which the node applies.
+        prediction (int): The predicted class for all of the instances to which 
+                          the node applies.
+        left (Node): The left child node of the current node.
+        right (Node): The right child node of the current node.
+    '''
+    def __init__(self, gini, decision, classes):
+        self.gini = gini
         self.decision = decision
         self.classes = classes
         self.prediction = np.argmax(classes)
         self.left = None
         self.right = None
         
-        
+
+
 class Tree(object):
-    def __init__(self):
+    '''
+    A decision tree is a tree-like model in which instances are classified based
+    on a series of attribute tests. Each instance moves from the root node, down 
+    the decision tree, until it reaches a leaf node at which point the instance
+    is classified. The path the instance follows to reach a leaf node is determined
+    based on the reesult of a set a predetermined attribute tests.
+    
+    Args:
+        None.
+    
+    Attributes:
+        root (Node): The root node of the decision tree.
+        decisions (list): A list storing all of the decision used
+    '''
+    def __init__(self, max_depth=np.inf, min_samples_split=2, min_samples_leaf=1):
         self.root = None
         
+        if max_depth > 0:
+            self.max_depth = max_depth
+        else:
+            raise AttributeError('Invalid max_depth value, max_depth must be greater than zero.')
+            
+        if min_samples_split > 1:
+            self.min_samples_split = min_samples_split
+        else:
+            raise AttributeError('Invalid min_samples_split value, min_samples_split must be greater than one.')
+        
+        if min_samples_leaf > 0:
+            self.min_samples_leaf = min_samples_leaf
+        else:
+            raise AttributeError('Invalid min_samples_leaf value, min_samples_leaf must be greater than zero.')
+        
+        
     def split(self, split_val, split_col, X, y):
+        '''Split the given dataset based on the given feature and threshold value.'''
         lower = []
         y_lower = []
         upper = []
         y_upper = []
         
-        #split
+        # Split the data at the based on the specified feature and threshold value.
         for i,x in enumerate(X[:,split_col]):
             if x < split_val:
                 lower.append(X[i])
@@ -34,14 +83,19 @@ class Tree(object):
             else:
                 upper.append(X[i])
                 y_upper.append(y[i])
-                
+        
+        # The separated instances.
         lower = np.array(lower)
         upper = np.array(upper)
+        # The labels corresponding to the separated instances.
         y_lower = np.array(y_lower).astype(int)
         y_upper = np.array(y_upper).astype(int)
+        
         return lower, y_lower, upper, y_upper   
     
+    
     def gini_impurity(self,y):
+        '''Calculate the Gini impurity of the specified node.'''
         ratio = np.bincount(y)
         total = ratio.sum()
         gini = 1
@@ -49,12 +103,15 @@ class Tree(object):
             gini -= (i/total)**2
         return gini
     
+    
     def CART(self, X,y):
+        '''CART algorithm for classification.'''
         m,n = X.shape
         splits = X.ravel(order='C')
         min_cost = np.inf
         ginis = []
         
+        # Testing all possible split points.
         for i,x in enumerate(splits):
             lower, y_lower, upper, y_upper = self.split(x, (i % 2), X, y)
             
@@ -67,58 +124,52 @@ class Tree(object):
             cost = gini_left + gini_right
             
             ginis.append((gini_left + gini_right, (x, (i % 2))))
+            # Store split that results in lowest value for CART cost function
+            # for classification.
             if cost < min_cost:
                 min_cost = cost
                 best_split = (x, (i % 2))
-        #print(best_split)        
+        
+        # Return the node that best classifies the given data set.
         return Node(min_cost, best_split, np.bincount(y))
         
 
-
-    def __fit2(self, subtree, X, y, classes):
-        if self.gini_impurity(y) == 0: 
+    def __fit(self, subtree, X, y, classes, curr_depth):
+        '''
+        If the dataset does not already have a Gini impurity of zero, create and
+        add a node to the decision tree that better classfies the instances in
+        the given dataset. The recursively call the _fit method to create the 
+        child nodes.
+        '''
+        if curr_depth > self.max_depth or len(X) < self.min_samples_split or len(X) < self.min_samples_leaf or self.gini_impurity(y) == 0: 
             return Node(0, None, np.bincount(y))
         else:
             subtree = self.CART(X,y)
             lower, y_lower, upper, y_upper = self.split(subtree.decision[0], 
                                                         subtree.decision[1], X, y)
             
-            subtree.left = self.__fit2(subtree.left, lower, y_lower, subtree.classes)
-            subtree.right = self.__fit2(subtree.right, upper, y_upper, subtree.classes)
+            subtree.left = self.__fit(subtree.left, lower, y_lower, subtree.classes, curr_depth + 1)
+            subtree.right = self.__fit(subtree.right, upper, y_upper, subtree.classes, curr_depth + 1)
             return subtree
-       
+        
+        
+        
     def fit(self,X,y):
+        '''
+        Build the decision tree by recursively calling the __fit method until
+        all of the training instances have been correctly classified or the specified
+        regularization criteria have been satisified.
+        '''
         self.root = self.CART(X,y)
     
         lower, y_lower, upper, y_upper = self.split(self.root.decision[0], 
                                                     self.root.decision[1], X, y)
+        self.root.left = self.__fit(self.root.left, lower, y_lower, np.bincount(y_lower), 1)
+        self.root.right = self.__fit(self.root.right, upper, y_upper, np.bincount(y_upper), 1)
         
-        self.root.left = self.__fit2(self.root.left, lower, y_lower, np.bincount(y_lower))
-        self.root.right = self.__fit2(self.root.right, upper, y_upper, np.bincount(y_upper))
-        # print(self.root.left.classes)
-        # print(self.root.right.classes)
-        
-            
-            
-            
-    def __traverse(self, subtree):
-        if subtree.left is not None:
-            self.__traverse(subtree.left)
-        #print(subtree.gini, end = ' ')
-        #print(subtree.decision, end = ' ')
-        print(subtree.decision, end = ' ')
-        if subtree.decision is not None:
-            self.decisions.append(subtree.decision)
-        if subtree.right is not None:
-            self.__traverse(subtree.right)
-           
-        
-    def traverse(self):
-        self.decisions = []
-        self.__traverse(self.root)
-        print()
         
     def __predict(self, subtree, val):
+        '''Predict the class of an instance.'''
         if subtree.decision is None:
             return subtree.prediction
         elif val[subtree.decision[1]] < subtree.decision[0]:
@@ -127,59 +178,62 @@ class Tree(object):
             return self.__predict(subtree.right, val)
         
     def predict(self, val):
-        return self.__predict(self.root,val)
+        '''Predict the class of an instnace.'''
+        #print(self.root.decision)
+        return self.__predict(self.root, val)
         
 
-data = make_blobs(n_samples=50, n_features=2, centers=2, cluster_std=5, 
-                  random_state=0)
-X = data[0]
-y = data[1]   
-
-train_set = X.copy() 
-labels = y.copy()
-
-X_one_min = X[:,0].min() * 1.1
-X_one_max = X[:,0].max() * 1.1
-X_two_min = X[:,1].min() * 1.1
-X_two_max = X[:,1].max() * 1.1
-
-tree = Tree()
-tree.fit(X,y)
 
 
-X1, X2 = np.meshgrid(np.linspace(X_one_min, X_one_max, 500), np.linspace(X_two_min, X_two_max,500))
-X = [[x1,x2] for x1,x2 in zip(X1.flatten(),X2.flatten())]
-y = lambda x: tree.predict(x)
-Y = []
-for x in X:
-    Y.append(y(x))
-
-Y = np.array(Y)
-Y = Y.reshape(X1.shape)
-
-fig = plt.gcf()
-fig.set_size_inches((10,7))
 
 
-cp1 = plt.contourf(X1, X2, Y, [-1,0, 1], cmap='winter')
-plt.scatter(train_set[:,0], train_set[:,1], c=labels, cmap='winter', edgecolors='black')
-#plt.scatter(X1, X2, c=Y, s=0.1)
 
+
+   
     
-plt.xlabel('Feature one (Dimension one)')
-plt.ylabel('Feature two (Dimension two)')
-plt.title(f'Decision boundary of a decision tree with no regularization:')
-plt.show()
-
-plt.show()
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # def __traverse(self, subtree):
+    #     if subtree.left is not None:
+    #         self.__traverse(subtree.left)
+    #     if subtree.decision is not None:
+    #         self.decisions.append(subtree.decision)
+    #     if subtree.right is not None:
+    #         self.__traverse(subtree.right)
+           
+        
+    # def traverse(self):
+    #     self.decisions = []
+    #     self.__traverse(self.root)
 
 
 
