9433da130e932574b2c1a7747ea83af94f5ae536
TristanBester <tristanbester@gmail.com>
2020-01-21 21:45:43 +0200

    Removed temp.py

--- mlgroundup/temp.py
+++
@@ -1,3 +0,0 @@
-from supervised import DecisionTreeRegressor, DecisionTreeClassifier, LinearRegression 
-from supervised import SupportVectorMachine
-from supervised import RidgeRegression, LassoRegression, ElasticNet, LogisticRegression
