6295ef1dfb9faf4d4f37a80dcf9b32bd6cbc8cad
TristanBester <46284695+TristanBester@users.noreply.github.com>
2020-03-29 14:08:08 +0200

    Updated comments in PCA

--- mlgroundup/unsupervised/principal_component_analysis.py
+++ mlgroundup/unsupervised/principal_component_analysis.py
@@ -6,14 +6,14 @@ import numpy as np
 class PCA(object):
     '''
     Principal Component Analysis is a dimensionality reduction technique. The axis
-    (one-dimensional hyperplane) that accounts for the maximum variance of the 
-    data is calculated then a second axis, orthogonal to the first, that accounts
-    for the maximum remaining variance of the data is calculated. This process of
-    repeatedly calculating the orthogonal axis that accounts for the largest
-    amount of the remaining variance is repeated until an axis has been calculated
-    for each dimension. These axes are the principal components of the data. The 
-    data can then be projected onto the selected number of principal components, 
-    with the data being projected onto the axes that account for more variance first.
+    that accounts for the maximum variance of the data is calculated then a second 
+    axis, orthogonal to the first, that accounts for the maximum remaining variance 
+    of the data is calculated. This process of repeatedly calculating the orthogonal
+    axis that accounts for the largest amount of the remaining variance is repeated 
+    until an axis has been calculated for each dimension. These axes are the principal
+    components of the data. The data can then be projected onto the selected number of
+    principal components, with the data being projected onto the axes that account for 
+    more variance first.
     
     Args:
         n_components: The number of principal components the data will be projected
